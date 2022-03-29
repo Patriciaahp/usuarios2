30b2de5b90c28cd97f77ae06253316170053b013
Patriciaahp <patrihernandezperez9@gmail.com>
2022-03-29 15:50:00 +0200

    Realizado ordenado por columnas

--- app/Http/Livewire/Users/UsersTable.php
+++ app/Http/Livewire/Users/UsersTable.php
@@ -14,9 +14,13 @@ class UsersTable extends Component
     public $selectedColumns = [];
     public $per_page=10;
     public $search;
+    public $sortColumn = "id";
+    public $sortDirection = "asc";
 
     protected $queryString = [
         'search' => ['except'=> ''],
+        'sortColumn' => [],
+        'sortDirection' => [],
     ];
     public function updatingSearch()
     {
@@ -26,15 +30,30 @@ class UsersTable extends Component
     {
         $this->resetPage();
     }
+    public function updatingSortColumn()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSortDirection()
+    {
+        $this->resetPage();
+    }
     public function mount()
     {
         $this->selectedColumns = $this->columns;
+    }
+    public function sort($column)
+    {
+        $this->sortColumn = $column;
+        $this->sortDirection = $this->sortDirection == 'asc' ? 'desc' : 'asc';
     }
     protected function getUsers(UserFilter $userFilter)
     {
         $users = User::query()->filterBy($userFilter, array_merge([
             'search' => $this->search,
         ]))
+            ->orderBy($this->sortColumn, $this->sortDirection)
             ->paginate($this->per_page);
         $users->appends($userFilter->valid());
 
--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -1,31 +1,28 @@
 <div>
     <div class="shadow p-3 mb-5 bg-body rounded row" >
         <h1 class="col">User List</h1>
-
         <div class="d-grid gap-2 d-md-flex justify-content-md-end col">
             <button href="#Filter" class="btn btn-primary" data-toggle="collapse" type="button">Filtrar</button>
         </div>
         <div class="d-grid gap-2 d-md-flex col">
         <button href="#Sort" class="btn btn-default" data-toggle="collapse">Ordenar</button>
         </div>
-
         <select class=" col form-select" aria-label="Default select example" wire:model="per_page">
             <option value="10">10</option>
             <option value="15">15</option>
             <option value="20">20</option>
             <option value="30">30</option>
         </select>
-
-        <label for="exampleDataList" class="form-label"></label>
-        <input name="name" wire:model="search" type="text" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-    </div>
-    <div>
+        </div>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end col">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+            </svg>
             <a href="{{ route('users.create') }}" class="btn btn-outline-success" type="button">New user</a>
 
         </div>
-    </div>
-<div class="container-sm">
+<div class="container-sm d-grid gap-2 d-md-flex col">
     <div id="Filter" class="collapse">
       <div>
           <h4>Mostrar/Ocultar:</h4>
@@ -36,9 +33,17 @@
       </div>
     </div>
     <div id="Sort" class="collapse">
-        DIV SORT
+        <button wire:click="sort('users.id')">Order by id</button>
+        <button wire:click="sort('users.name')">Order by name</button>
+        <button wire:click="sort('users.surname')">Order by surname</button>
+        <button wire:click="sort('users.email')">Order by email</button>
     </div>
 </div>
+    <div class="container-sm">
+        <label for="exampleDataList" class="form-label"></label>
+        <input name="name" wire:model="search" type="text" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+
+    </div>
 
     <table class="table table-hover" >
         <thead>
