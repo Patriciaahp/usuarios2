1a58752f6ce7deef43adcf87b8b00aa98f52d3a7
Patriciaahp <patrihernandezperez14@gmail.com>
2022-04-05 15:06:32 +0200

    Refactorizada vista tabla

--- resources/views/users/livewire/users-table.blade.php
+++ resources/views/users/livewire/users-table.blade.php
@@ -1,12 +1,14 @@
 <div>
     <div class=" container-xxl shadow p-3 mb-5 bg-body rounded">
         <h1>User List</h1>
-
         @include('users.shared._filters')
     </div>
     <div class="container-sm">
-        @include('users.shared._table')
+        @if($users->count())
+            @include('users.shared._table')
+        @else
+            <h3>Not found users... </h3>
+        @endif
         {{ $users->links() }}
     </div>
-
 </div>
