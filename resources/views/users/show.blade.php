f1430b0929325f2504bed22192a01cfd6bd94af2
Patriciaahp <patrihernandezperez14@gmail.com>
2022-04-07 12:59:58 +0200

    Cambiado modal detalles de usuario, hay que arreglar el modal para borrar usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,15 +1,15 @@
 <div>
     <div class="modal-header">
         <h2 class="modal-title">User Details</h2>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
     <div class="modal-body">
         <h1 class="col"></h1>
         @include('users.shared._row')
     </div>
     <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">
-            Close
-        </button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
     </div>
-</div>
 </div>
