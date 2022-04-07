a5cd40b838bea00682254ccf6347a741b9fd63cd
Patriciaahp <patrihernandezperez14@gmail.com>
2022-04-07 14:46:37 +0200

    Mejorado modal para borrar

--- resources/views/users/preview.blade.php
+++ resources/views/users/preview.blade.php
@@ -2,21 +2,24 @@
     <div>
         <div class="text-center container-sm modal-header row">
             <h1 class="col ">Are you sure?</h1>
+            <button type="button" class="close col col-2" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
         <div class="modal-body">
             @include('users.shared._row')
         </div>
-        <div class="container-sm modal-footer">
-            <div class="row col">
+        <div class="container-sm modal-footer row">
+            <button type="button" class="btn btn-primary col col-2" data-dismiss="modal" aria-label="Close">
+                Close
+            </button>
+            <div class="col">
                 <form action="{{ route('delete',['id' => $user->id]) }}" method="POST">
                     <input name="_method" type="hidden" value="DELETE">
                     @csrf
                     <button type="submit" class="btn btn-outline-danger">Yes, I want to delete this user</button>
                 </form>
             </div>
-            <button type="button" class="btn btn-primary" data-dismiss="modal" aria-label="Close">
-                Close
-            </button>
         </div>
     </div>
 </div>
