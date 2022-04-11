7790aac54cb651cb160f02d1c3185a46f8ae48c7
Patriciaahp <patrihernandezperez14@gmail.com>
2022-04-11 12:12:19 +0200

    Mejorada vista login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,27 +1,41 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="card">
-                <h3 class="card-header text-center">Login</h3>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('log') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="name">Email:</label>
-                            <input type="text" name="email" id="email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Password:</label>
-                            <input type="password" name="password" id="password" class="form-control">
-                        </div>
+<div class="container-fluid teal">
+    <div class=" container-lg row ">
+        <div class="container-sm col">
+            <h1 class="text-center">Welcome</h1>
+        </div>
+        <div class="container-sm col white">
+            <h3 class=" text-center">Login</h3>
+            <div class="">
+                <form method="POST" action="{{ route('log') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Email:</label>
+                        <input type="text" name="email" id="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Password:</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
 
-                        <div>
-                            <button type="submit" class="btn btn-success">Sing in</button>
-                        </div>
-                    </form>
-
-                </div>
+                    <div>
+                        <button type="submit" class="btn btn-success">Sing in</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
+<style>
+    .teal {
+        background-color: #20c997;
+    }
+
+    .white {
+        background-color: #ffffff;
+    }
+</style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
